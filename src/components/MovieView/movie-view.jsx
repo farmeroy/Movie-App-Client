d86dc22c92a6ef69121689c5decac134a2a007c7
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-02 13:33:20 +0100

    corrected movie object to match database

--- src/components/MovieView/movie-view.jsx
+++ src/components/MovieView/movie-view.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 
 class MovieView extends React.Component {
+
   render() {
     const { movie, onBackClick } = this.props;
     return (
       <div className="movie-view">
-        <div className="movie-poster">
-          <img src={movie.ImagePath} />
-        </div>
+        {/* <div className="movie-poster"> */}
+        {/*   <img src={movie.ImagePath} /> */}
+        {/* </div> */}
         <div className="movie-title">
           <span className="label">Title:</span>
           <span className="value">{movie.Title}</span>
         </div>
         <div className="movie-director">
           <span className="label">Director:</span>
-          <span className="value">{movie.Director}</span>
+          <span className="value">{movie.Director.Name}</span>
         </div>
         <div className="movie-description">
           <span className="label">Description:</span>
