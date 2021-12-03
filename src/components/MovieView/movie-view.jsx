84d3871f62613deec767bc4f6053a84cb02e6e98
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-03 10:41:16 +0100

    changed image path name -- still not building

--- src/components/MovieView/movie-view.jsx
+++ src/components/MovieView/movie-view.jsx
@@ -7,9 +7,9 @@ class MovieView extends React.Component {
     const { movie, onBackClick } = this.props;
     return (
       <div className="movie-view">
-        {/* <div className="movie-poster"> */}
-        {/*   <img src={movie.ImagePath} /> */}
-        {/* </div> */}
+        <div className="movie-poster">
+          <img src={`../img/${movie.ImagePath}`} />
+        </div>
         <div className="movie-title">
           <span className="label">Title:</span>
           <span className="value">{movie.Title}</span>
