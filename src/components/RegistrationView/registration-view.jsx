ec0c6ab41e9a0a8f03288d3be5a497e62fa07e67
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-03 08:59:18 +0100

    created registeration view

---
+++ src/components/RegistrationView/registration-view.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+function RegistrationView(props) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthday, setBirthday] = useState("");
+
+  return (
+    <form>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </label>
+      <label>
+        Birthday:
+        <input
+          type="date"
+          value={birthday}
+          onChange={(event) => setBirthday(event.target.value)}
+        />
+      </label>
+      <button type="submit" onClick={console.log('clicked')}>
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default RegistrationView;
+
