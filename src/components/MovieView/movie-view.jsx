dc28f1a1fc4b196ca6593e946537e4b312fe08e8
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-07 00:25:24 +0100

    refactored with bootstrap components

--- src/components/MovieView/movie-view.jsx
+++ src/components/MovieView/movie-view.jsx
@@ -1,29 +1,35 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import { Row, Col, Button, Image, Card, ListGroup } from "react-bootstrap";
 
 class MovieView extends React.Component {
-
   render() {
     const { movie, onBackClick } = this.props;
     return (
-      <div className="movie-view">
-        <div className="movie-poster">
-          <img src={movie.ImagePath} />
-        </div>
-        <div className="movie-title">
-          <span className="label">Title:</span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-director">
-          <span className="label">Director:</span>
-          <span className="value">{movie.Director.Name}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description:</span>
-          <span className="value">{movie.Description}</span>
-        </div>
-        <button onClick={onBackClick}>Back</button>
-      </div>
+      <Row className="movie-view">
+        <Col md={6} className="movie-poster">
+          <Image src={movie.ImagePath} fluid/>
+        </Col>
+        <Col sm={10} md={6} >
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item className="movie-title">
+                <span className="label">Title: </span>
+                <span className="value">{movie.Title}</span>
+              </ListGroup.Item>
+              <ListGroup.Item className="movie-director">
+                <span className="label">Director: </span>
+                <span className="value">{movie.Director.Name}</span>
+              </ListGroup.Item>
+              <ListGroup.Item className="movie-description">
+                <span className="label">Description: </span>
+                <span className="value">{movie.Description}</span>
+              </ListGroup.Item>
+              <Button onClick={onBackClick}>Back</Button>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
     );
   }
 }
@@ -32,10 +38,9 @@ MovieView.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
-    ImagePath: PropTypes.string
+    ImagePath: PropTypes.string,
   }).isRequired,
-  onBackClick: PropTypes.func.isRequired
+  onBackClick: PropTypes.func.isRequired,
 };
 
 export default MovieView;
-
