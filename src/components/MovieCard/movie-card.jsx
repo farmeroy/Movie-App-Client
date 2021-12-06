fa8ce44d90079d442c0eb072b6387a3b4c93eb5f
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-06 10:31:00 +0100

    refactored with bootstrap

--- src/components/MovieCard/movie-card.jsx
+++ src/components/MovieCard/movie-card.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 class MovieCard extends React.Component {
   render() {
     const { movieData, onMovieClick } = this.props;
-    return <div className="movie-card"  onClick={ () => { onMovieClick(movieData); }} >{movieData.Title}</div>; }
+
+    return (
+      <Card>
+        <Card.Img variant="top" src={movieData.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movieData.Title}</Card.Title>
+          <Card.Text>{movieData.Description}</Card.Text>
+          <Button onClick={ () =>  onMovieClick(movieData)} variant="link">Open</Button>
+          </Card.Body>
+      </Card>
+    );
+  }
 }
 
 MovieCard.propTypes = {
