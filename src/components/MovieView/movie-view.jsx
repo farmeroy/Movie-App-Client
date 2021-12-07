931b87f77652ebe452873e9b2bc9bbceb288d560
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-07 11:01:17 +0100

    basic movie info styling

--- src/components/MovieView/movie-view.jsx
+++ src/components/MovieView/movie-view.jsx
@@ -1,31 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Row, Col, Button, Image, Card, ListGroup } from "react-bootstrap";
+import './movie-view.scss';
 
 class MovieView extends React.Component {
   render() {
     const { movie, onBackClick } = this.props;
     return (
-      <Row className="movie-view">
+        <Row className="movie-view"> 
         <Col sm={12} md={6} className="movie-poster">
-          <Image src={movie.ImagePath} fluid/>
+          <Image src={movie.ImagePath} fluid />
         </Col>
         <Col sm={12} md={6} >
-          <Card>
+          <Card bsPrefix='movie-info'>
             <ListGroup variant="flush">
-              <ListGroup.Item className="movie-title">
+              <ListGroup.Item className="movie-info">
                 <span className="label">Title: </span>
                 <span className="value">{movie.Title}</span>
               </ListGroup.Item>
-              <ListGroup.Item className="movie-director">
+              <ListGroup.Item className="movie-info">
                 <span className="label">Director: </span>
                 <span className="value">{movie.Director.Name}</span>
               </ListGroup.Item>
-              <ListGroup.Item className="movie-description">
+              <ListGroup.Item className="movie-info">
                 <span className="label">Description: </span>
                 <span className="value">{movie.Description}</span>
               </ListGroup.Item>
-              <Button onClick={onBackClick}>Back</Button>
+              <Button onClick={onBackClick} className="back-btn">Back</Button>
             </ListGroup>
           </Card>
         </Col>
