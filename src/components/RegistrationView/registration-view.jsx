6fee6cdeb8c8d77e412b7bb2532c702b99c93703
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-07 12:36:50 +0100

    added col width

--- src/components/RegistrationView/registration-view.jsx
+++ src/components/RegistrationView/registration-view.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { Form, Button } from "react-bootstrap";
+import { Col, Form, Button } from "react-bootstrap";
 
 function RegistrationView(props) {
   const [username, setUsername] = useState("");
@@ -9,41 +9,53 @@ function RegistrationView(props) {
   const [birthday, setBirthday] = useState("");
 
   return (
-    <Form>
-      <Form.Group controlId="formUserName">
-        <Form.Label><span className="label">Username:</span></Form.Label>
-        <Form.Control
-          type="text"
-          onChange={(event) => setUsername(event.target.value)}
-        />
-      </Form.Group>
+    <Col md={8}>
+      <Form>
+        <Form.Group controlId="formUserName">
+          <Form.Label>
+            <span className="label">Username:</span>
+          </Form.Label>
+          <Form.Control
+            type="text"
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </Form.Group>
 
-      <Form.Group controlId="formPassword">
-        <Form.Label><span className="label">Password</span></Form.Label>
-        <Form.Control
-          type="password"
-          onChange={(event) => setPassword(event.target.value)}
-        />
-      </Form.Group>
+        <Form.Group controlId="formPassword">
+          <Form.Label>
+            <span className="label">Password</span>
+          </Form.Label>
+          <Form.Control
+            type="password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </Form.Group>
 
-      <Form.Group controlId="formEmail">
-        <Form.Label><span className="label">Email:</span></Form.Label>
-        <Form.Control
-          type="text"
-          onchange={(event) => setEmail(event.target.value)}
-        />
-      </Form.Group>
+        <Form.Group controlId="formEmail">
+          <Form.Label>
+            <span className="label">Email:</span>
+          </Form.Label>
+          <Form.Control
+            type="text"
+            onchange={(event) => setEmail(event.target.value)}
+          />
+        </Form.Group>
 
-      <Form.Group controlId="formBirthday">
-        <Form.Label><span className="label">Birthday:</span></Form.Label>
-        <Form.Control
-          type="date"
-          onChange={(event) => setBirthday(event.target.value)}
-        />
-      </Form.Group>
+        <Form.Group controlId="formBirthday">
+          <Form.Label>
+            <span className="label">Birthday:</span>
+          </Form.Label>
+          <Form.Control
+            type="date"
+            onChange={(event) => setBirthday(event.target.value)}
+          />
+        </Form.Group>
 
-      <Button type="submit" onClick={() => console.log('clicked')}>Confirm</Button>
-    </Form>
+        <Button type="submit" onClick={() => console.log("clicked")}>
+          Confirm
+        </Button>
+      </Form>
+    </Col>
   );
 }
 
