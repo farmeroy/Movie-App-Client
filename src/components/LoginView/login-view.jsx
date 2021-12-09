3edc60aee5fe8c036e9930733e7121046b4f842e
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-09 14:46:43 +0100

    useNavigate to registration route

--- src/components/LoginView/login-view.jsx
+++ src/components/LoginView/login-view.jsx
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import axios from 'axios';
 import {Button, Col, Form} from "react-bootstrap";
+import { useNavigate } from 'react-router-dom';
 import './login-view.scss';
 
 function LoginView(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-
+  const navigate = useNavigate();
   const handleSubmit = (event) => {
     event.preventDefault();
     axios.post('https://pre-code-flix.herokuapp.com/login', {
@@ -47,7 +48,7 @@ function LoginView(props) {
       <Button sizevariant="primary" type="submit" onClick={handleSubmit}>
         Login
       </Button>{' '}
-      <Button variant="secondary" type="button" onClick={props.onRegisterUser}>
+      <Button variant="secondary" type="button" onClick={()=>{navigate('/registration')}}>
         New Account
       </Button>
     </Form>
