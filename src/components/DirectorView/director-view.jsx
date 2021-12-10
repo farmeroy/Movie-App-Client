3a34f8a0d841ee3cc692a3e5c8327633c5911d7a
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-10 14:02:31 +0100

    added <Carc> component

--- src/components/DirectorView/director-view.jsx
+++ src/components/DirectorView/director-view.jsx
@@ -1,15 +1,30 @@
 import { React } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import { Row, Card, Col, Button } from "react-bootstrap";
 
 const DirectorView = (props) => {
   const { movies } = props;
   const { name } = useParams();
-  console.log("movies:", movies);
-  const directorData = movies.find(
+  const navigate = useNavigate();
+  const director = movies.find(
     (movie) => movie.Director.Name === name
   ).Director;
-  console.log("director", directorData);
-  return <p>{directorData.Name}</p>;
+  return (
+    <Row>
+      <Card>
+        <Card.Title>{director.Name}</Card.Title>
+        <Card.Text>{director.Bio}</Card.Text>
+        <Button
+          onClick={() => {
+            navigate("/");
+          }}
+          className="back-btn"
+        >
+          Back
+        </Button>
+      </Card>
+    </Row>
+  );
 };
 
 export default DirectorView;
