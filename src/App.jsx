0d28ab3287b74d577d8ea9b424a2fd388ca4bc4b
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-10 14:12:07 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./index.scss";
+// import MainView from "./components/MainView/main-view";
+import MainView from "./components/MainView/main-view.func.jsx";
+import Container from "react-bootstrap/Container";
+
+const App = () => {
+    return (
+      <Container className="app-background">
+        <MainView />
+      </Container>
+
+    )}
+
+export default App;
