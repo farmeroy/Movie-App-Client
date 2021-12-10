02ca37bab1057c4558ed4384300f7ea940113af7
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-10 14:11:20 +0100

    refactored to render a function component/ added BrowserRouter

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.scss";
-// import MainView from "./components/MainView/main-view";
-import MainView from "./components/MainView/main-view.func.jsx";
-import Container from "react-bootstrap/Container";
+import {render} from 'react-dom'
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
-class MyFlixApp extends React.Component {
-  render() {
-    return (
-      <Container className="app-background">
-        <MainView />
-      </Container>
-    );
-  }
-}
+const rootElement = document.getElementById('root');
+render(
+<BrowserRouter>
+<App />
+</BrowserRouter>,
+ rootElement);
 
-// Find the app root
-const container = document.getElementsByClassName("app-container")[0];
-
-// Tell React to render your app in the root DOM element
-ReactDOM.render(React.createElement(MyFlixApp), container);
