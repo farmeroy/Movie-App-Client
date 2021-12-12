c585992d4130360b7737df08179d11ea0d8aee65
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-12 16:46:34 +0100

    added form validation

--- src/components/LoginView/login-view.jsx
+++ src/components/LoginView/login-view.jsx
@@ -1,20 +1,46 @@
 import React, { useState } from "react";
+import useForm from "../../hooks/useForm";
 import PropTypes from "prop-types";
 import axios from 'axios';
 import {Button, Col, Form} from "react-bootstrap";
 import { useNavigate } from 'react-router-dom';
 import './login-view.scss';
 
+// validation logic to check input in text field
+const textInputIsValid = (text) => {
+  return text.trim() !== "";
+}
+
 function LoginView(props) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+
   const navigate = useNavigate();
+
+  const {
+    enteredValue: enteredUsername,
+    hasError: usernameHasError,
+    isValid: usernameIsValid,
+    inputChangeHandler: usernameChangeHandler,
+    inputTouchHandler: usernameTouchHandler
+  } = useForm(textInputIsValid);
+
+  const {
+    enteredValue: enteredPassword,
+    hasError: passwordHasError,
+    isValid: passwordIsValid,
+    inputChangeHandler: passwordChangeHandler,
+    inputTouchHandler: passwordTouchHandler
+  } = useForm(textInputIsValid);
+
+  const formIsValid = usernameIsValid && passwordIsValid;
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    if (!formIsValid) {
+      return;
+    }
     axios.post('https://pre-code-flix.herokuapp.com/login', {
-      Username: username,
-      Password: password
+      Username: enteredUsername,
+      Password: enteredPassword
     })
       .then(response => { 
         const data = response.data;
@@ -32,18 +58,25 @@ function LoginView(props) {
         <Form.Label><span className='label'>Username:</span></Form.Label>
         <Form.Control
           type="text"
-          onChange={(event) => setUsername(event.target.value)}
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+          onBlur={usernameTouchHandler}
           placeholder='username'
-        />
+        />       
+        {usernameHasError && <p>Please enter your Username</p>}
       </Form.Group>
 
       <Form.Group controlId="formPassword">
         <Form.Label><span className='label'>Password:</span></Form.Label>
         <Form.Control
           type="password"
-          onChange={(event) => setPassword(event.target.value)}
+          value={enteredPassword}
+          onChange={passwordChangeHandler}
+          onBlur={passwordTouchHandler}
+          onChange={passwordChangeHandler}
           placeholder='password'
         />
+        {passwordHasError && <p>Please enter your Password</p>}
       </Form.Group>
 
       <Button sizevariant="primary" type="submit" onClick={handleSubmit}>
