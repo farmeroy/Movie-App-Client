8eb4dfe3e5db220a9ee93dee177fb6d80474a268
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-12 19:09:32 +0100

    added form validation with useForm hook

--- src/components/RegistrationView/registration-view.jsx
+++ src/components/RegistrationView/registration-view.jsx
@@ -1,24 +1,67 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import useForm from '../../hooks/useForm';
 import { Col, Form, Button } from "react-bootstrap";
 import axios from 'axios';
 
+// validation logic to check input in text field
+const textInputIsValid = (text) => {
+  return text.trim() !== "";
+}
+
 function RegistrationView(props) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [birthday, setBirthday] = useState("");
+
+  const {
+    enteredValue: enteredEmail,
+    hasError: emailHasError,
+    isValid: emailIsValid,
+    inputChangeHandler: emailChangeHandler,
+    inputTouchHandler: emailTouchHandler,
+  } = useForm(textInputIsValid);
+
+  const {
+    enteredValue: enteredBirthday,
+    hasError: birthdayHasError,
+    isValid: birthdayIsValid,
+    inputChangeHandler: birthdayChangeHandler,
+    inputTouchHandler: birthdayTouchHandler
+  } = useForm(textInputIsValid);
+
+  const {
+    enteredValue: enteredUsername,
+    hasError: usernameHasError,
+    isValid: usernameIsValid,
+    inputChangeHandler: usernameChangeHandler,
+    inputTouchHandler: usernameTouchHandler
+  } = useForm(textInputIsValid);
+
+  const {
+    enteredValue: enteredPassword,
+    hasError: passwordHasError,
+    isValid: passwordIsValid,
+    inputChangeHandler: passwordChangeHandler,
+    inputTouchHandler: passwordTouchHandler
+  } = useForm(textInputIsValid);
+
+  const formIsValid = usernameIsValid && passwordIsValid && emailIsValid && birthdayIsValid;
+
 
 
   const registerUserHandler = (event) => {
     event.preventDefault();
+    if (!formIsValid) {
+      usernameTouchHandler();
+      passwordTouchHandler();
+      emailTouchHandler();
+      birthdayTouchHandler();
+      return;
+    }
     const newUser = {
-      Username: username,
-      Password: password,
-      Email: email,
-      Birthday: new Date(birthday)
+      Username: enteredUsername,
+      Password: enteredPassword,
+      Email: enteredEmail,
+      Birthday: new Date(enteredBirthday)
     };
-    console.log(newUser, typeof newUser.Birthday)
 
     axios.post('https://pre-code-flix.herokuapp.com/users', newUser )
       .then(response => {
@@ -34,14 +77,18 @@ function RegistrationView(props) {
   return (
     <Col md={8}>
       <Form>
-        <Form.Group controlId="formUserName">
+        <Form.Group controlId="formUsername">
           <Form.Label>
             <span className="label">Username:</span>
           </Form.Label>
           <Form.Control
             type="text"
-            onChange={(event) => setUsername(event.target.value)}
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+            onBlur={usernameTouchHandler}
+            placeholder="username"
           />
+          {usernameHasError && <p>Please enter a Username</p>}
         </Form.Group>
 
         <Form.Group controlId="formPassword">
@@ -50,8 +97,12 @@ function RegistrationView(props) {
           </Form.Label>
           <Form.Control
             type="password"
-            onChange={(event) => setPassword(event.target.value)}
+            value={enteredPassword}
+            onChange={passwordChangeHandler}
+            onBlur={passwordTouchHandler}
+            placeholder='password'
           />
+          {passwordHasError && <p>Please enter a password</p>}
         </Form.Group>
 
         <Form.Group controlId="formEmail">
@@ -60,8 +111,12 @@ function RegistrationView(props) {
           </Form.Label>
           <Form.Control
             type="text"
-            onChange={(event) => setEmail(event.target.value)}
+            placeholder="email"
+            value={enteredEmail}
+            onChange={emailChangeHandler}
+            onBlur={emailTouchHandler}
           />
+          {emailHasError && <p>Please enter a valid email</p>}
         </Form.Group>
 
         <Form.Group controlId="formBirthday">
@@ -70,8 +125,12 @@ function RegistrationView(props) {
           </Form.Label>
           <Form.Control
             type="date"
-            onChange={(event) => setBirthday(event.target.value)}
+            placeholder=''
+            value={enteredBirthday}
+            onBlur={birthdayTouchHandler}
+            onChange={birthdayChangeHandler}
           />
+          {birthdayHasError && <p>Please enter your birthday</p>}
         </Form.Group>
 
         <Button type="submit" onClick={registerUserHandler}>
