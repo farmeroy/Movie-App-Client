7aed245ab2db68516115bd1317d3d0057d943830
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-12 16:49:19 +0100

    added card stylings

--- src/components/UserProfileView/user-info.jsx
+++ src/components/UserProfileView/user-info.jsx
@@ -1,14 +1,16 @@
 import { React, useState } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import UpdateUserForm from './update-user-form.jsx'; 
+import './user-profile-view.css';
+
 
 const UserInfo = (props) => {
   const { userData } = props;
   const [ showUpdateUserForm, setShowUpdateUserForm ] = useState(false);
 
   return (
-    <Card>
-      <Card.Title>{userData.Username}</Card.Title>
+    <Card bsPrefix='profile-card'>
+      <Card.Title bsPrefix='limelight'>{userData.Username}</Card.Title>
       <Card.Text>{userData.Email}</Card.Text>
       { !showUpdateUserForm &&
       <Button onClick={()=>{setShowUpdateUserForm(true)}}>Update Info</Button>
