04f84e7fb53eed923c4259c7c2153d73677562b8
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-13 09:50:08 +0100

    added userData prop

--- src/components/DirectorView/director-view.jsx
+++ src/components/DirectorView/director-view.jsx
@@ -1,15 +1,23 @@
 import { React } from "react";
+import Movies from "../Movies/movies";
 import { useParams, useNavigate } from "react-router-dom";
 import { Row, Card, Col, Button } from "react-bootstrap";
 
 const DirectorView = (props) => {
-  const { movies } = props;
+  const { userData, movies } = props;
   const { name } = useParams();
   const navigate = useNavigate();
+
   const director = movies.find(
     (movie) => movie.Director.Name === name
   ).Director;
+  
+  const directorMovies = movies.filter(
+    (movie) => movie.Director.Name === name
+  );
+
   return (
+    <>
     <Row>
       <Card>
         <Card.Title>{director.Name}</Card.Title>
@@ -24,6 +32,10 @@ const DirectorView = (props) => {
         </Button>
       </Card>
     </Row>
+    <Row>
+      <Movies moviesData={directorMovies} userData={userData} /> 
+    </Row>
+    </>
   );
 };
 
