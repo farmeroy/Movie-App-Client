c746b714d73b60701778caab3c5ab495bd671c8d
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-13 08:15:21 +0100

    filter for other movies in the genre, display those movies

--- src/components/GenreView/genre-view.jsx
+++ src/components/GenreView/genre-view.jsx
@@ -1,32 +1,43 @@
-import { React } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { React } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import Movies from "../Movies/movies.jsx";
 import { Row, Card, Col, Button } from "react-bootstrap";
 
 const GenreView = (props) => {
-  const { movies } = props;
+  const { movies, userData } = props;
   const { name } = useParams();
   const navigate = useNavigate();
 
-  const genre = movies.find( movie => movie.Genre.Name === name ).Genre;
+  const genre = movies.find((movie) => movie.Genre.Name === name).Genre;
+
+  const genreMovies = movies.filter(movie => movie.Genre.Name === name);
+
+  console.log(name );
 
   return (
-
-    <Row>
-      <Card>
-        <Card.Title>{genre.Name}</Card.Title>
-        <Card.Text>{genre.Bio}</Card.Text>
-        <Button
-          onClick={() => {
-            navigate("/");
-          }}
-          className="back-btn"
-        >
-          Back
-        </Button>
-      </Card>
-    </Row>
+    <>
+      <Row>
+        <Card>
+          <Card.Title>{genre.Name}</Card.Title>
+          <Card.Text>{genre.Bio}</Card.Text>
+          <Button
+            onClick={() => {
+              navigate("/");
+            }}
+            className="back-btn"
+          >
+            Back
+          </Button>
+        </Card>
+      </Row>
+      <Row>
+        <h1>Other {genre.Name} Movies</h1>
+        </Row>
+      <Row>
+        <Movies moviesData={genreMovies} userData={userData} />
+      </Row>
+    </>
   );
-}
-
+};
 
 export default GenreView;
