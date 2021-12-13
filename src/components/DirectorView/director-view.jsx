a57338bd16ddfdcd306f306ba1717277555759a4
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-13 13:21:20 +0100

    added styles

--- src/components/DirectorView/director-view.jsx
+++ src/components/DirectorView/director-view.jsx
@@ -2,6 +2,7 @@ import { React } from "react";
 import Movies from "../Movies/movies";
 import { useParams, useNavigate } from "react-router-dom";
 import { Row, Card, Col, Button } from "react-bootstrap";
+import './director-view.scss';
 
 const DirectorView = (props) => {
   const { userData, movies } = props;
@@ -19,8 +20,8 @@ const DirectorView = (props) => {
   return (
     <>
     <Row>
-      <Card>
-        <Card.Title>{director.Name}</Card.Title>
+      <Card bsPrefix="director-card">
+        <Card.Title bsPrefix="limelight">{director.Name}</Card.Title>
         <Card.Text>{director.Bio}</Card.Text>
         <Button
           onClick={() => {
@@ -31,6 +32,11 @@ const DirectorView = (props) => {
           Back
         </Button>
       </Card>
+    </Row>
+    <Row>
+      <Col sm={12}>
+        <h1 className='limelight other-films'>Other Films by {director.Name}</h1>
+      </Col>
     </Row>
     <Row>
       <Movies moviesData={directorMovies} userData={userData} /> 
