42b99a0a4b8dc6a36802331704ad30f208fa28c6
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-13 08:17:04 +0100

    http requests now working

--- src/components/MovieCard/movie-card-func.jsx
+++ src/components/MovieCard/movie-card-func.jsx
@@ -3,8 +3,8 @@ import "./movie-card.scss";
 import PropTypes from "prop-types";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const truncateText = (text) => {
   if (!text) {
@@ -18,49 +18,56 @@ const truncateText = (text) => {
   return text;
 };
 
-
 const MovieCard = (props) => {
   const { movieData, userData } = props;
   const navigate = useNavigate();
   const movieId = movieData._id;
 
-  const isUserFav = userData.FavMovies.find(movie => movie._id === movieId);
-  
+  const isUserFav = userData.FavMovies.find((movie) => movie._id === movieId);
+
   const removeFavHandler = () => {
     const username = localStorage.getItem("user");
     const movieId = movieData._id;
-    const token = localStorage.getItem("token")
+    const token = localStorage.getItem("token");
     console.log(token, username);
     axios
-      .put(`http://pre-code-flix.herokuapp.com/users/${username}/movies/remove/${movieId}`, {
-        headers: { Authorization: `Bearer ${token}` },
-        method: 'PUT'
-      })
+      .put(
+        `http://pre-code-flix.herokuapp.com/users/${username}/movies/remove/${movieId}`,
+        {},
+        {
+          headers: { Authorization: `Bearer ${token}` },
+          method: "PUT",
+        }
+      )
       .then((response) => {
-        console.log(response)
+        console.log(response);
       })
       .catch((error) => {
-        console.log(error)
+        console.log(error);
       });
   };
 
   const addFavHandler = () => {
-    const username = localStorage.getItem('user');
+    const username = localStorage.getItem("user");
     const token = localStorage.getItem("token");
     const movieId = movieData._id;
-    console.log(movieId)
+    console.log(movieId);
     // console.log(token)
-      axios
-       .put(`http://pre-code-flix.herokuapp.com/users/${username}/movies/${movieId}`, {
-         headers: { Authorization: `Bearer ${token}` },
-       })
-       .then((response) => {
-         console.log(response)
-       })
-       .catch((error) => {
-         console.log(error)
-       })
-    console.log('add')
+    axios
+      .put(
+        `http://pre-code-flix.herokuapp.com/users/${username}/movies/${movieId}`,
+        {},
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    console.log("add");
   };
 
   return (
@@ -69,11 +76,14 @@ const MovieCard = (props) => {
       <Card.Body>
         <Card.Title bsPrefix="limelight">{movieData.Title}</Card.Title>
         <Card.Text>{truncateText(movieData.Description)}</Card.Text>
-        <Button onClick={() => navigate(`/movies/${movieId}`)} variant="link-dark">
+        <Button
+          onClick={() => navigate(`/movies/${movieId}`)}
+          variant="link-dark"
+        >
           Open
         </Button>
-        {isUserFav && <Button onClick={removeFavHandler}>Unlike</Button>} 
-       {!isUserFav && <Button onClick={addFavHandler}>Like</Button>}
+        {isUserFav && <Button onClick={removeFavHandler}>Unlike</Button>}
+        {!isUserFav && <Button onClick={addFavHandler}>Like</Button>}
       </Card.Body>
     </Card>
   );
