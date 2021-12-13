4d1b7241a9193bfc3b1775ea633367026836cd94
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-13 12:52:26 +0100

    added delete user button and http request

--- src/components/UserProfileView/user-info.jsx
+++ src/components/UserProfileView/user-info.jsx
@@ -1,23 +1,56 @@
-import { React, useState } from 'react';
-import { Button, Card } from 'react-bootstrap';
-import UpdateUserForm from './update-user-form.jsx'; 
-import './user-profile-view.css';
-
+import { React, useState } from "react";
+import { Button, Card } from "react-bootstrap";
+import UpdateUserForm from "./update-user-form.jsx";
+import "./user-profile-view.css";
+import axios from "axios";
 
 const UserInfo = (props) => {
   const { userData } = props;
-  const [ showUpdateUserForm, setShowUpdateUserForm ] = useState(false);
+  const [showUpdateUserForm, setShowUpdateUserForm] = useState(false);
+
+  const deleteUserHandler = () => {
+    const token = localStorage.getItem("token");
+    const user = localStorage.getItem("user");
+
+    axios
+      .delete(`http://pre-code-flix.herokuapp.com/users/${user}/delete`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        console.log(response);
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        window.open("/", "_self");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
-    <Card bsPrefix='profile-card'>
-      <Card.Title bsPrefix='limelight'>{userData.Username}</Card.Title>
+    <Card bsPrefix="profile-card">
+      <Card.Title bsPrefix="limelight">{userData.Username}</Card.Title>
       <Card.Text>{userData.Email}</Card.Text>
-      { !showUpdateUserForm &&
-      <Button onClick={()=>{setShowUpdateUserForm(true)}}>Update Info</Button>
-      }
-      {showUpdateUserForm && <UpdateUserForm userData={userData} hideForm={ () => {setShowUpdateUserForm(false)}}/>}
+      {!showUpdateUserForm && (
+        <Button
+          onClick={() => {
+            setShowUpdateUserForm(true);
+          }}
+        >
+          Update Info
+        </Button>
+      )}
+      {showUpdateUserForm && (
+        <UpdateUserForm
+          userData={userData}
+          hideForm={() => {
+            setShowUpdateUserForm(false);
+          }}
+        />
+      )}
+      <Button onClick={deleteUserHandler}>Delete Account</Button>
     </Card>
-  )
-}
+  );
+};
 
 export default UserInfo;
