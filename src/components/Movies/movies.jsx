da3bc11dcea7ac78fa15174e23ef9d25bfaa1656
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-15 09:36:22 +0100

    added search feature

--- src/components/Movies/movies.jsx
+++ src/components/Movies/movies.jsx
@@ -1,17 +1,40 @@
 import React from "react";
+import { connect } from "react-redux";
 import MovieCard from "../MovieCard/movie-card.jsx";
+import VisibilityFilterInput from "../VisibilityFilterInput/visibility-filter-input";
 import { Col } from "react-bootstrap";
 
-const Movies = (props) => {
-  const { moviesData, userData } = props;
+const mapStateToProps = (state) => {
+  const { visibilityFilter, movies} = state;
+  return { visibilityFilter, movies };
+};
 
-  const movieCards = moviesData.map((movie) => (
+const Movies = (props) => {
+  const { movies, userData, visibilityFilter } = props;
+  let filteredMovies = movies;
+
+  if (visibilityFilter !== "") {
+    filteredMovies = movies.filter((movie) =>
+      movie.Title.toLowerCase().includes(visibilityFilter.toLowerCase())
+    );
+  }
+
+  if (!movies) return <div className="main-view" />;
+
+  const movieCards = filteredMovies.map((movie) => (
     <Col sm={6} md={4} lg={3} className="movie-column" key={movie._id}>
-      <MovieCard key={movie._id} movieData={movie} userData={userData}/>
+      <MovieCard key={movie._id} movieData={movie} userData={userData} />
     </Col>
   ));
 
-  return <>{movieCards}</>;
+  return (
+    <>
+      <Col md={12} style={{ margin: "1em" }}>
+        <VisibilityFilterInput visibilityFilter={visibilityFilter} />
+      </Col>
+      {movieCards}
+    </>
+  );
 };
 
-export default Movies;
+export default connect(mapStateToProps)(Movies);
