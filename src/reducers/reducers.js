f6d5f2b8c0749ff99c85dacac7831f15c658dee1
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-15 09:34:35 +0100

    added setUserData

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { SET_FILTER, SET_MOVIES } from '../actions/actions';
+import { SET_USER_DATA, SET_FILTER, SET_MOVIES } from '../actions/actions';
 
 function visibilityFilter(state = '', action) {
   switch (action.type) {
@@ -19,9 +19,19 @@ function movies(state = [], action) {
   }
 }
 
+function userData(state = '', action) {
+  switch (action.type) {
+    case SET_USER_DATA:
+      return action.value;
+    default:
+      return state;
+  }
+}
+
 const moviesApp = combineReducers({
   visibilityFilter,
-  movies
+  movies,
+  userData
 });
 
 export default moviesApp;
