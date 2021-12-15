6a7d3adbe033f52ddc5037e220ea1a6be7226c1e
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-15 21:44:16 +0100

    basic confirm delete modal implemented

--- src/components/UserProfileView/user-info.jsx
+++ src/components/UserProfileView/user-info.jsx
@@ -1,18 +1,18 @@
 import { React, useState } from "react";
-import { Button, Card } from "react-bootstrap";
+import { Modal, Button, Card } from "react-bootstrap";
 import UpdateUserForm from "./update-user-form.jsx";
 import "./user-profile-view.css";
 import axios from "axios";
 import { connect } from "react-redux";
 
 const mapStateToProps = (state) => {
-  return { userData: state.userData }
+  return { userData: state.userData };
 };
 
 const UserInfo = (props) => {
   const { userData } = props;
   const [showUpdateUserForm, setShowUpdateUserForm] = useState(false);
-
+  const [showDeleteUser, setShowDeleteUser] = useState(false);
   const deleteUserHandler = () => {
     const token = localStorage.getItem("token");
     const user = localStorage.getItem("user");
@@ -31,6 +31,9 @@ const UserInfo = (props) => {
         console.log(error);
       });
   };
+
+  const hideDeleteHandler = () => setShowDeleteUser(false);
+  const showDeleteUserHandler = () => setShowDeleteUser(true);
 
   return (
     <Card bsPrefix="profile-card">
@@ -52,7 +55,21 @@ const UserInfo = (props) => {
           }}
         />
       )}
-      <Button onClick={deleteUserHandler}>Delete Account</Button>
+      <Button onClick={showDeleteUserHandler}>Delete Account</Button>
+      <Modal show={showDeleteUser} onHide={hideDeleteHandler}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete Account</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Are you sure you want to delete your account? This cannot be undone...
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={hideDeleteHandler}>
+            Cancel
+          </Button>
+          <Button onClick={deleteUserHandler}>Delete Account</Button>
+        </Modal.Footer>
+      </Modal>
     </Card>
   );
 };
