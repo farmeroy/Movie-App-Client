0ac55b084b5ded87b2e29fc66046483a87e023c7
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-15 09:46:03 +0100

    now renders movie cards directly, using redux

--- src/components/UserProfileView/fav-movies.jsx
+++ src/components/UserProfileView/fav-movies.jsx
@@ -1,16 +1,41 @@
-import { React } from 'react';
-import Movies from '../Movies/movies';
+import React from "react";
+import { connect } from "react-redux";
+import MovieCard from "../MovieCard/movie-card.jsx";
+import VisibilityFilterInput from "../VisibilityFilterInput/visibility-filter-input";
+import { Col } from "react-bootstrap";
 
+const mapStateToProps = (state) => {
+  const { visibilityFilter, userData} = state;
+  return { visibilityFilter, userData};
+};
 
 const FavMovies = (props) => {
-  const { userData, moviesData } = props;
-  const userFavMovies = [];
+  const { userData, visibilityFilter } = props;
+  let filteredMovies = userData.FavMovies;
 
-  userData.FavMovies.forEach( (userMovie) => {
-    userFavMovies.push(moviesData.find(movie => movie._id === userMovie._id))
-  });
-  
-  return <Movies moviesData={userFavMovies} userData={userData}/>
-}
+  if (visibilityFilter !== "") {
+    filteredMovies = movies.filter((movie) =>
+      movie.Title.toLowerCase().includes(visibilityFilter.toLowerCase())
+    );
+  }
 
-export default FavMovies;
+  if (!userData) return <div className="main-view" />;
+
+  const movieCards = filteredMovies.map((movie) => (
+    <Col sm={6} md={4} lg={3} className="movie-column" key={movie._id}>
+      <MovieCard key={movie._id} movieData={movie} userData={userData} />
+    </Col>
+  ));
+
+  return (
+    <>
+      <Col md={12} style={{ margin: "1em" }}>
+        <VisibilityFilterInput visibilityFilter={visibilityFilter} />
+      </Col>
+      {movieCards}
+    </>
+  );
+};
+
+export default connect(mapStateToProps)(FavMovies);
+
