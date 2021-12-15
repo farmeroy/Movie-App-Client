4ac5411220f707d11788c942d90d742d183d6d33
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-15 10:18:09 +0100

    movies are derived from state and movie cards rendered directl in component

--- src/components/DirectorView/director-view.jsx
+++ src/components/DirectorView/director-view.jsx
@@ -1,8 +1,14 @@
 import { React } from "react";
-import Movies from "../Movies/movies";
+import { connect } from "react-redux";
+import MovieCard from "../MovieCard/movie-card.jsx";
 import { useParams, useNavigate } from "react-router-dom";
 import { Row, Card, Col, Button } from "react-bootstrap";
-import './director-view.scss';
+import "./director-view.scss";
+
+const mapStateToProps = (state) => {
+  const { movies, userData } = state;
+  return { movies, userData };
+};
 
 const DirectorView = (props) => {
   const { userData, movies } = props;
@@ -12,37 +18,41 @@ const DirectorView = (props) => {
   const director = movies.find(
     (movie) => movie.Director.Name === name
   ).Director;
-  
-  const directorMovies = movies.filter(
-    (movie) => movie.Director.Name === name
-  );
+
+  const directorMovies = movies.filter((movie) => movie.Director.Name === name);
+
+  const movieCards = directorMovies.map((movie) => (
+    <Col sm={6} md={4} lg={3} className="movie-column" key={movie._id}>
+      <MovieCard key={movie._id} movieData={movie} userData={userData} />
+    </Col>
+  ));
 
   return (
     <>
-    <Row>
-      <Card bsPrefix="director-card">
-        <Card.Title bsPrefix="limelight">{director.Name}</Card.Title>
-        <Card.Text>{director.Bio}</Card.Text>
-        <Button
-          onClick={() => {
-            navigate("/");
-          }}
-          className="back-btn"
-        >
-          Back
-        </Button>
-      </Card>
-    </Row>
-    <Row>
-      <Col sm={12}>
-        <h1 className='limelight other-films'>Other Films by {director.Name}</h1>
-      </Col>
-    </Row>
-    <Row>
-      <Movies moviesData={directorMovies} userData={userData} /> 
-    </Row>
+      <Row>
+        <Card bsPrefix="director-card">
+          <Card.Title bsPrefix="limelight">{director.Name}</Card.Title>
+          <Card.Text>{director.Bio}</Card.Text>
+          <Button
+            onClick={() => {
+              navigate(-1);
+            }}
+            className="back-btn"
+          >
+            Back
+          </Button>
+        </Card>
+      </Row>
+      <Row>
+        <Col sm={12}>
+          <h1 className="limelight other-films">
+            Other Films by {director.Name}
+          </h1>
+        </Col>
+      </Row>
+      <Row>{movieCards}</Row>
     </>
   );
 };
 
-export default DirectorView;
+export default connect(mapStateToProps)(DirectorView);
--- src/components/GenreView/genre-view.jsx
+++ src/components/GenreView/genre-view.jsx
@@ -1,8 +1,14 @@
 import { React } from "react";
+import { connect } from "react-redux";
 import { useParams, useNavigate } from "react-router-dom";
-import Movies from "../Movies/movies.jsx";
+import MovieCard from "../MovieCard/movie-card.jsx";
 import { Row, Card, Col, Button } from "react-bootstrap";
-import './genre-view.scss';
+import "./genre-view.scss";
+
+const mapStateToProps = (state) => {
+  const { movies, userData } = state;
+  return { movies, userData };
+};
 
 const GenreView = (props) => {
   const { movies, userData } = props;
@@ -11,36 +17,42 @@ const GenreView = (props) => {
 
   const genre = movies.find((movie) => movie.Genre.Name === name).Genre;
 
-  const genreMovies = movies.filter(movie => movie.Genre.Name === name);
+  const genreMovies = movies.filter((movie) => movie.Genre.Name === name);
+
+  const movieCards = genreMovies.map((movie) => (
+    <Col sm={6} md={4} lg={3} className="movie-column" key={movie._id}>
+      <MovieCard key={movie._id} movieData={movie} userData={userData} />
+    </Col>
+  ));
 
   return (
     <>
       <Row>
         <Col sm={12}>
-        <Card bsPrefix="genre-card">
-          <Card.Title bsPrefix="limelight">{genre.Name}</Card.Title>
-          <Card.Text>{genre.Description}</Card.Text>
-          <Button
-            onClick={() => {
-              navigate("/");
-            }}
-            className="back-btn"
-          >
-            Back
-          </Button>
-        </Card>
+          <Card bsPrefix="genre-card">
+            <Card.Title bsPrefix="limelight">{genre.Name}</Card.Title>
+            <Card.Text>{genre.Description}</Card.Text>
+            <Button
+              onClick={() => {
+                navigate(-1);
+              }}
+              className="back-btn"
+            >
+              Back
+            </Button>
+          </Card>
         </Col>
       </Row>
       <Row>
         <Col sm={12}>
-        <h1 className="other-films limelight">Other {genre.Name} Movies</h1>
+          <h1 className="other-films limelight">Other {genre.Name} Movies</h1>
         </Col>
-        </Row>
+      </Row>
       <Row className="main-view justify-content-md-center row-eq-height">
-        <Movies moviesData={genreMovies} userData={userData} />
+        {movieCards}
       </Row>
     </>
   );
 };
 
-export default GenreView;
+export default connect(mapStateToProps)(GenreView);
